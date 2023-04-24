e71975d75b15f7645f18dcf3aefbd22e4081d37a
FlaviaAMorgado <127445372+FlaviaAMorgado@users.noreply.github.com>
2023-04-24 08:09:53 -0300

    Add files via upload

---
+++ Aula/script.js
@@ -0,0 +1,9 @@
+/* let msg=confirm("Você é um aluno?")
+document.write("veja o resultado ", msg) 
+
+let msg1=prompt("Digite seu nome")
+document.write("o valor digitado foi: ", msg1)*/
+
+function trocar(cor){
+    document.body.style.background=cor
+}
